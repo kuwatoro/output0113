306f7f22fd69847952188464cae8bc0d2e6a28bb
kuwatoro <rx.1.13ayumu@icloud.com>
2021-03-23 02:52:35 +0900

    ruby問題６回答と解説

---
+++ ruby_mondaisyuu/ruby_6.rb
@@ -0,0 +1,14 @@
+#weather = "晴れ"
+
+#def get_weather_forecast(weather)
+#  puts "明日の天気は #{weather} です。"
+#end
+
+#get_weather_forecast(weather)
+
+
+def get_weather_forecast(weather)
+  puts "明日の天気は #{weather} です。"
+end
+
+get_weather_forecast("晴れ")
